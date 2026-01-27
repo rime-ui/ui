b2b03347d2402e389fc5db5b5aab8f7d343fcb0c
Tno <msa@moh-sa.dev>
2026-01-27 20:42:35 +0300

    feat(ui): add `TabsContent` component to `tabs`

---
+++ packages/ui/src/components/tabs/tabs-content/tabs-content.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { tv, VariantProps } from "tailwind-variants";
+import { useTabsContext } from "../tabs-context";
+
+
+const content = tv({
+    base: "py-4",
+})
+
+type TabsContentProps = React.HTMLAttributes<HTMLDivElement> & VariantProps<typeof content> & { value: string };
+
+
+export function TabsContent(props: TabsContentProps) {
+    const { activeTab } = useTabsContext();
+
+    if (activeTab !== props.value) return null;
+
+    return <div className={content({ className: props.className })}>{props.children}</div>;
+}
