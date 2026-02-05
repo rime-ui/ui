fb24f0b38c25739c01d4910e3229d68d762e0f5c
Tno <msa@moh-sa.dev>
2026-02-05 22:21:35 +0300

    feat(ui): add `Card` component to encapsulate card-related sub-components

---
+++ packages/ui/src/components/card/card.tsx
@@ -0,0 +1,13 @@
+import { CardBody } from './card-body/card-body'
+import { CardFooter } from './card-footer/card-footer'
+import { CardHeader } from './card-header/card-header'
+import { CardRoot } from './card-root/card-root'
+import { CardTitle } from './card-title/card-title'
+
+export const Card = {
+    Root: CardRoot,
+    Body: CardBody,
+    Footer: CardFooter,
+    Header: CardHeader,
+    Title: CardTitle,
+}
