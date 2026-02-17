99577872f2377e3ee6550a2874a385c35650f5ff
Mahmoud_Walid <modywmbadr@gmail.com>
2026-02-17 16:10:25 +0200

    feat(docs): add custom Duolingo icon support in source configuration
    Add an icon plugin handler to render a custom Duolingo SVG icon when the icon identifier is 'duolingo'. This allows for branded icon integration within the documentation navigation.

--- apps/docs/src/lib/source.ts
+++ apps/docs/src/lib/source.ts
@@ -1,12 +1,27 @@
 import { docs } from 'fumadocs-mdx:collections/server'
 import { type InferPageType, loader } from 'fumadocs-core/source'
 import { lucideIconsPlugin } from 'fumadocs-core/source/lucide-icons'
+import React from 'react'
 
 // See https://fumadocs.dev/docs/headless/source-api for more info
 export const source = loader({
     baseUrl: '/docs',
     source: docs.toFumadocsSource(),
     plugins: [lucideIconsPlugin()],
+    icon(icon) {
+        if (!icon) {
+            return
+        }
+
+        if (icon === 'duolingo') {
+            return React.createElement('img', {
+                src: '/icons/duolingo.svg',
+                alt: 'Duolingo',
+                width: 16,
+                height: 16,
+            })
+        }
+    },
 })
 
 export function getPageImage(page: InferPageType<typeof source>) {
